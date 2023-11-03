b3372b69739b349d431184a7937e364bde9026aa
Robin van der Noord <robinvandernoord@gmail.com>
2023-11-03 15:01:58 +0100

    fix: use new `get_task` function and updated pip_* function

--- src/edwh_whitelabel_plugin/whitelabel_plugin.py
+++ src/edwh_whitelabel_plugin/whitelabel_plugin.py
@@ -1,29 +1,42 @@
-import io
+import sys
 
-from invoke import task
+from edwh import get_task
 from edwh_pipcompile_plugin.pipcompile_plugin import DEFAULT_SERVER
-from edwh_pipcompile_plugin.pipcompile_plugin import compile_infile as pip_compile
-from edwh_pipcompile_plugin.pipcompile_plugin import upgrade as pip_upgrade
+from invoke import task
 
 
-@task(aliases=("pip-compile",))
-def compile(ctx, path, pypi_server=DEFAULT_SERVER):
+@task
+def setup(c):
+    print("Whitelabel specific setup")
+
+
+@task(name="compile", aliases=("pip-compile",))
+def wl_pip_compile(ctx, path, pypi_server=DEFAULT_SERVER):
     if not path.endswith(".in"):
         # normally, look at requirements.in but for whitelabel look at whitelabel.in and platform.in
         path = [f"{path}/whitelabel.in", f"{path}/platform.in"]
+
+    if not (pip_compile := get_task("pip.compile")):
+        print("! pip.compile task not found !", file=sys.stderr)
+        return
 
     return pip_compile(
         ctx,
         path,
         pypi_server=pypi_server,
+        combine=True,
     )
 
 
-@task(aliases=("pip-upgrade",))
-def upgrade(ctx, path, package=None, force=False, pypi_server=DEFAULT_SERVER):
+@task(name="upgrade", aliases=("pip-upgrade",))
+def wl_pip_upgrade(ctx, path, package=None, force=False, pypi_server=DEFAULT_SERVER):
     if not path.endswith(".in"):
         # normally, look at requirements.in but for whitelabel look at whitelabel.in and platform.in
         path = [f"{path}/whitelabel.in", f"{path}/platform.in"]
+
+    if not (pip_upgrade := get_task("pip.upgrade")):
+        print("! pip.upgrade task not found !", file=sys.stderr)
+        return
 
     return pip_upgrade(
         ctx,
@@ -31,4 +44,5 @@ def upgrade(ctx, path, package=None, force=False, pypi_server=DEFAULT_SERVER):
         package=package,
         force=force,
         pypi_server=pypi_server,
+        combine=True,
     )
